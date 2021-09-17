cd4f9624db3dc49cd98398a0081044b6fee47c54
Kaio <luizkaio9815@gmail.com>
2021-09-17 13:52:27 -0300

    enviando arquivos com as atividades sobre javascript

---
+++ script1.js
@@ -0,0 +1,30 @@
+//Exercicio 2 parte 1
+var comida = "Lasanha";
+console.log (comida);
+
+var number = 20;
+console.log (number);
+
+//Exercicio 2 parte 2
+let dado = null;
+
+dado = 5 * 3;
+console.log (dado);
+
+let nome = "Matheus";
+console.log (nome + " é meu amigo.");
+
+nome = "Inara";
+console.log (nome + " é minha amiga.");
+
+nome = "Camila";
+console.log (nome + " também é minha amiga.");
+
+//Exercicio 2 parte 3
+const GRAOS = ["arroz", "cafe", "feijao", "soja"];
+console.log (GRAOS);
+
+const MARCAS = ["Coca-Cola", "BMW", "Vitarella", "Lenovo", "Samsung"];
+console.log (MARCAS)
+
+
---
+++ script2.js
@@ -0,0 +1,42 @@
+//Atividade de Fixação
+
+//a) 
+var number = 20;
+
+console.log (number);
+
+//b)
+var string = "texto aleatório";
+
+console.log (string);
+
+//c)
+var ehVerdade = true;
+var ehMentira = false;
+
+console.log ("O dia foi bom? " + ehMentira);
+console.log ('amanhã vai ser melhor? ' + ehVerdade);
+
+//d)
+var weekDays = ["Sunday", "Monday", "Thuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+console.log (weekDays);
+
+//e)
+var carro = {year: 2020, color: "green", model: "Gol"};
+
+console.log (carro);
+console.log (carro.model);
+console.log (carro.year);
+console.log (carro.color);
+
+//f)
+var value_undefined;
+
+console.log("Valor não definido = " + value_undefined)
+
+//g)
+
+var value_empty = null;
+
+console.log("Valor nulo = " + value_empty)
